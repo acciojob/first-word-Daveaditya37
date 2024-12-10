383f66944c195c169b696ca4b5c3421186f7f856
Daveaditya37 <160773920+Daveaditya37@users.noreply.github.com>
2024-12-10 09:41:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function firstWord(s) {
-  // your code here
+	if (s=='') {
+		return s;
+	}
+  for (let i = 0; i < s.length; i++) {
+	  let ch+=s[i];
+  	if (s==' ') {
+  		return ch;
+  	}else{
+		return s;
+	}
+  }
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
